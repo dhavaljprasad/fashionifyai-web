fc3a792663f60e34065e8b1f0dc939e87bdaf5bc
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-10 01:16:33 +0530

    feat: added new model card for profile page

---
+++ src/components/profile/model-card.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+type ModelCardType = {
+  name: string;
+  model_id: string;
+  image_url: string;
+  gender: "male" | "female";
+};
+
+export const ModelCard = ({
+  data,
+  onClick,
+}: {
+  data: ModelCardType;
+  onClick: (id: string) => void;
+}) => {
+  return (
+    <div
+      className="w-full h-auto flex flex-col items-center justify-center"
+      onClick={() => onClick(data.model_id)}
+    >
+      <img className="w-full aspect-[2/3] object-cover" src={data.image_url} />
+      <div className="w-full flex items-center justify-between bg-background-secondary p-4">
+        <h1 className="text-sm font-semibold text-contrast">{data.name}</h1>
+        <span className="text-xs text-contrast">{data.gender}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ModelCard;
