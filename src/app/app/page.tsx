4d349755f981ebb391d593db9d68e3a413a51de2
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-11 04:03:47 +0530

    update: updated /app page with new components

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -4,10 +4,12 @@ import { useRouter } from "next/navigation";
 import { AppPageHeader } from "@/components/app-page/header";
 import { SideBar } from "@/components/modular/side-bar";
 import { ModelCard, ModelCardDataType } from "@/components/app-page/model-card";
+import { SegmentedFloatingButton } from "@/components/app-page/segmented-float-button";
 import { api } from "@/lib/api";
 import { useAuth } from "../providers/auth";
 import { upload } from "@imagekit/next";
 import { Check, X, Images } from "lucide-react";
+import { CreativeInputBox } from "@/components/app-page/creative-input";
 
 const NEXT_PUBLIC_IMGKIT_PUBLIC_KEY =
   process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY || "";
@@ -19,6 +21,9 @@ function AppPage() {
   const [userModels, setUserModels] = useState<ModelCardDataType[]>([]);
   const [userModelSelected, setUserModelSelected] = useState<boolean>(false);
   const [uploading, setUploading] = useState(false);
+  const [activeSection, setActiveSection] = useState<
+    "visualization" | "ai stylist"
+  >("visualization");
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -253,93 +258,105 @@ function AppPage() {
         setShowSidebar={() => setShowSidebar(!showSidebar)}
       />
       {showSidebar && <SideBar />}
-      <div className="flex h-full w-full flex-col items-center justify-between pt-22 sm:flex-row sm:justify-center sm:gap-4 sm:pt-24">
-        {capturedImage ? (
-          <div className="relative aspect-[2/3] h-fit max-h-[70dvh] sm:max-h-[90dvh]">
-            <img src={capturedImage} className="h-full w-full object-cover" />
-            <div
-              className={`absolute -bottom-12 flex h-24 w-full items-center justify-around ${uploading ? "opacity-50" : ""}`}
-            >
+      {activeSection == "visualization" ? (
+        <div className="flex h-full w-full flex-col items-center justify-between pt-22 sm:flex-row sm:justify-center sm:gap-4 sm:pt-24">
+          {capturedImage ? (
+            <div className="relative aspect-[2/3] h-fit max-h-[70dvh] sm:max-h-[90dvh]">
+              <img src={capturedImage} className="h-full w-full object-cover" />
               <div
-                className="flex h-24 w-24 items-center justify-center bg-contrast"
-                onClick={() => onDiscardImage()}
+                className={`absolute -bottom-12 flex h-24 w-full items-center justify-around ${uploading ? "opacity-50" : ""}`}
               >
-                <X className="text-accent" />
-              </div>
-              <div
-                className="flex h-24 w-24 items-center justify-center bg-accent"
-                onClick={() => onConfirmImage()}
-              >
-                <Check className="text-contrast" />
+                <div
+                  className="flex h-24 w-24 items-center justify-center bg-contrast"
+                  onClick={() => onDiscardImage()}
+                >
+                  <X className="text-accent" />
+                </div>
+                <div
+                  className="flex h-24 w-24 items-center justify-center bg-accent"
+                  onClick={() => onConfirmImage()}
+                >
+                  <Check className="text-contrast" />
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="relative aspect-[2/3] h-fit max-h-[70dvh] sm:max-h-[100dvh]">
-            <video
-              ref={videoRef}
-              autoPlay
-              playsInline
-              className="h-full w-full object-cover"
-            />
-            <div className="absolute -bottom-12 left-1/2 h-24 w-full -translate-x-1/2 flex flex-row items-center justify-evenly">
-              <div
-                className="h-24 w-24  bg-accent"
-                onClick={() => captureImage()}
+          ) : (
+            <div className="relative aspect-[2/3] h-fit max-h-[70dvh] sm:max-h-[100dvh]">
+              <video
+                ref={videoRef}
+                autoPlay
+                playsInline
+                className="h-full w-full object-cover"
               />
-              <div
-                className="h-24 w-24 bg-contrast flex items-center justify-center"
-                onClick={() => inputRef.current?.click()}
-              >
-                <Images className=" text-accent" size={24} />
+              <div className="absolute -bottom-12 left-1/2 h-24 w-full -translate-x-1/2 flex flex-row items-center justify-evenly">
+                <div
+                  className="h-24 w-24  bg-accent"
+                  onClick={() => captureImage()}
+                />
+                <div
+                  className="h-24 w-24 bg-contrast flex items-center justify-center"
+                  onClick={() => inputRef.current?.click()}
+                >
+                  <Images className=" text-accent" size={24} />
+                </div>
               </div>
             </div>
-          </div>
-        )}
-        <input
-          ref={inputRef}
-          type="file"
-          accept="image/*"
-          className="hidden"
-          onChange={handleFile}
-        />
+          )}
+          <input
+            ref={inputRef}
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleFile}
+          />
 
-        <div className="w-full h-auto sm:h-[70dvh] flex flex-col items-start justify-start mt-16 sm:mt-0 gap-2">
-          {userModels.length > 0 && (
-            <h1 className="text-center text-sm font-semibold text-text m-0">
-              REGISTERED MODELS
-            </h1>
-          )}
-          {userModels.length > 0 ? (
-            <div className="scrollbar-thin flex gap-2 overflow-auto scrollbar-thumb-text scrollbar-track-transparent">
-              {userModels.map((img, index) => (
-                <ModelCard
-                  data={img}
-                  key={index}
-                  selectImage={() => userModelSelectionFunc(img.image_url)}
-                />
-              ))}
-            </div>
-          ) : null}
-          {recentImages.length > 0 && (
-            <h1 className="text-center text-sm font-semibold text-text m-0">
-              RECENT PHOTOS
-            </h1>
-          )}
-          {recentImages.length > 0 ? (
-            <div className="scrollbar-thin flex gap-2 overflow-auto scrollbar-thumb-text scrollbar-track-transparent sm:overlfolow-none sm:flex-wrap">
-              {recentImages.map((img, index) => (
-                <img
-                  key={index}
-                  src={img}
-                  className="w-20 cursor-pointer object-cover"
-                  onClick={() => setCapturedImage(img)}
-                />
-              ))}
-            </div>
-          ) : null}
+          <div className="w-full h-auto sm:h-[70dvh] flex flex-col items-start justify-start mt-16 sm:mt-0 gap-2">
+            {userModels.length > 0 && (
+              <h1 className="text-center text-sm font-semibold text-text m-0">
+                REGISTERED MODELS
+              </h1>
+            )}
+            {userModels.length > 0 ? (
+              <div className="scrollbar-thin flex gap-2 overflow-auto scrollbar-thumb-text scrollbar-track-transparent">
+                {userModels.map((img, index) => (
+                  <ModelCard
+                    data={img}
+                    key={index}
+                    selectImage={() => userModelSelectionFunc(img.image_url)}
+                  />
+                ))}
+              </div>
+            ) : null}
+            {recentImages.length > 0 && (
+              <h1 className="text-center text-sm font-semibold text-text m-0">
+                RECENT PHOTOS
+              </h1>
+            )}
+            {recentImages.length > 0 ? (
+              <div className="scrollbar-thin flex gap-2 overflow-auto scrollbar-thumb-text scrollbar-track-transparent sm:overlfolow-none sm:flex-wrap">
+                {recentImages.map((img, index) => (
+                  <img
+                    key={index}
+                    src={img}
+                    className="w-20 cursor-pointer object-cover"
+                    onClick={() => setCapturedImage(img)}
+                  />
+                ))}
+              </div>
+            ) : null}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex h-full w-full flex items-center justify-center pt-22 sm:pt-24">
+          <CreativeInputBox />
+        </div>
+      )}
+      <SegmentedFloatingButton
+        selected={activeSection}
+        onChange={(n: string) =>
+          setActiveSection(n as "visualization" | "ai stylist")
+        }
+      />
     </div>
   );
 }
