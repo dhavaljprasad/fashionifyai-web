740d71245174b523513d9d9bcde13e6bc5627dec
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-11 04:03:28 +0530

    feat: made floating toggle and creative-input components for /app page

---
+++ src/components/app-page/creative-input.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const CreativeInputBox = () => {
+  return (
+    <div className="w-[90vw] h-auto border border-accent">
+      <input type="text" className="w-full" />
+    </div>
+  );
+};
---
+++ src/components/app-page/segmented-float-button.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+type Segment = "visualization" | "ai stylist";
+
+interface SegmentedFloatingButtonProps {
+  selected: Segment;
+  onChange: (value: Segment) => void;
+}
+
+const options: { label: string; value: Segment }[] = [
+  {
+    label: "Visualization",
+    value: "visualization",
+  },
+  {
+    label: "AI Stylist",
+    value: "ai stylist",
+  },
+];
+
+export const SegmentedFloatingButton = ({
+  selected,
+  onChange,
+}: SegmentedFloatingButtonProps) => {
+  return (
+    <div className="absolute bottom-12 left-1/2 -translate-x-1/2 flex items-center bg-background-secondary  shadow-lg">
+      {options.map((option) => {
+        const isSelected = selected === option.value;
+
+        return (
+          <button
+            key={option.value}
+            type="button"
+            onClick={() => onChange(option.value)}
+            className={`p-3 transition-all duration-200 cursor-pointer ${
+              isSelected
+                ? "bg-contrast text-background-primary"
+                : "text-accent hover:bg-background-tertiary"
+            }`}
+          >
+            <span className="font-semibold text-sm sm:text-base">
+              {option.label}
+            </span>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
