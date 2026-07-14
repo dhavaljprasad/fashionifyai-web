974a81a155f62dd2078ca522517ee111e72d465f
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-15 04:14:57 +0530

    update: updated allowed regex to now support extreamly wide range of fashion brands of India

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,10 +1,10 @@
 /**
- * Validates if a string is a shopping product URL from Amazon India or Flipkart
+ * Validates if a string is a supported shopping product URL
  * @param url - The URL string to validate
- * @returns true if the URL is from amazon.in, amzn.in, or flipkart.com; false otherwise
+ * @returns true if the URL is from a supported shopping domain; false otherwise
  */
 export const isShoppingProductUrl = (url: string): boolean => {
   const shoppingUrlPattern =
-    /^https:\/\/(www\.)?(amzn\.in|amazon\.in|flipkart\.com|dl\.flipkart\.com)(\/|$)/i;
+    /^https:\/\/(www\.)?(amzn\.in|amazon\.in|flipkart\.com|dl\.flipkart\.com|wforwoman\.com|shopforaurelia\.com|wishfulbyw\.com|elleven\.in|biba\.in|thesouledstore\.com|savana\.com|myntra\.com|nykaafashion\.com)(\/|$)/i;
   return shoppingUrlPattern.test(url);
 };
