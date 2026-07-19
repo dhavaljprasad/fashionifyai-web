8e657e977ae7d663de61f9f04dd8264fc0a992dc
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-20 03:47:46 +0530

    update: updated design to look much metter than prev chunky look

--- src/components/conversation-page/input-box.tsx
+++ src/components/conversation-page/input-box.tsx
@@ -9,13 +9,13 @@ interface InputBoxProps {
 
 export const InputBox = ({ value, setValue }: InputBoxProps) => {
   return (
-    <div className="fixed bottom-4 z-200 h-16 w-full px-4 sm:px-16">
+    <div className="fixed bottom-4 z-5 h-14 w-full px-4 sm:px-16">
       <div className="flex h-full w-full items-center justify-center gap-2 bg-white/80 p-2 transition-colors duration-200 focus-within:bg-white">
         <input
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          className="flex-1 bg-transparent text-xl font-semibold text-background-primary outline-none"
+          className="flex-1 bg-transparent text-base sm:text-lg font-semibold text-background-primary outline-none"
         />
         <div className="flex aspect-square h-full items-center justify-center bg-accent">
           <SendHorizontal size={24} className="text-white" />
