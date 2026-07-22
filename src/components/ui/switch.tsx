30d6981b550253e2522f594722b20078e1215b99
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-22 11:27:07 +0530

    feat: added switch toggle

---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Switch as SwitchPrimitive } from "@base-ui/react/switch";
+
+import { cn } from "@/lib/utils";
+
+function Switch({
+  className,
+  size = "default",
+  ...props
+}: SwitchPrimitive.Root.Props & {
+  size?: "sm" | "default";
+}) {
+  return (
+    <SwitchPrimitive.Root
+      data-slot="switch"
+      data-size={size}
+      className={cn(
+        "peer group/switch relative inline-flex shrink-0 items-center rounded-full border border-transparent transition-all outline-none after:absolute after:-inset-x-3 after:-inset-y-2",
+
+        // Focus
+        "focus-visible:ring-2 focus-visible:ring-accent/40",
+
+        // Sizes
+        "data-[size=default]:h-[18.4px] data-[size=default]:w-[32px]",
+        "data-[size=sm]:h-[14px] data-[size=sm]:w-[24px]",
+
+        // States
+        "data-checked:bg-accent",
+        "data-unchecked:bg-background-secondary",
+
+        // Disabled
+        "data-disabled:cursor-not-allowed data-disabled:opacity-50",
+
+        className,
+      )}
+      {...props}
+    >
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className={cn(
+          "pointer-events-none block rounded-full transition-transform ring-0",
+
+          // Thumb color
+          "bg-contrast",
+
+          // Sizes
+          "group-data-[size=default]/switch:size-4",
+          "group-data-[size=sm]/switch:size-3",
+
+          // Movement
+          "group-data-[size=default]/switch:data-checked:translate-x-[calc(100%-2px)]",
+          "group-data-[size=sm]/switch:data-checked:translate-x-[calc(100%-2px)]",
+          "group-data-[size=default]/switch:data-unchecked:translate-x-0",
+          "group-data-[size=sm]/switch:data-unchecked:translate-x-0",
+        )}
+      />
+    </SwitchPrimitive.Root>
+  );
+}
+
+export { Switch };
