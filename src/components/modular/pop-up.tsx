bac599f7bcd06f60c0fd2a67eb20aa3a117ff606
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-31 00:01:17 +0530

    update: made some subtle changes in pop-up component as well

--- src/components/modular/pop-up.tsx
+++ src/components/modular/pop-up.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { X } from "lucide-react";
 
-type PopUpSizeType = "small" | "large";
+export type PopUpSizeType = "small" | "large";
 
 export const PopUp = ({
   size,
@@ -17,7 +17,7 @@ export const PopUp = ({
   return (
     <div className="fixed inset-0 z-[10000] flex items-center justify-center bg-background-secondary/50 p-3 sm:p-6">
       <div
-        className={`flex flex-col overflow-hidden bg-background-primary ${size === "small" ? "h-64 w-96" : "h-[90vh] sm:h-fit w-[90vw] max-h-[900px] max-w-[1200px]"}`}
+        className={`flex flex-col overflow-hidden bg-background-primary ${size === "small" ? "h-64 w-96" : "h-[90vh] sm:h-fit w-[90vw] max-h-[900px] max-w-[1200px] w-fit"}`}
       >
         <div className="flex h-14 w-full items-center justify-between p-4">
           <h1 className="text-xl font-bold text-contrast sm:text-2xl m-0">
@@ -29,8 +29,8 @@ export const PopUp = ({
             onClick={closePopUp}
           />
         </div>
-        <div className="min-h-0 w-full h-fit overflow-hidden p-2 sm:p-4">
-          <div className="h-full w-full overflow-auto">{component}</div>
+        <div className="min-h-0 w-fit h-fit overflow-hidden p-2 sm:p-4">
+          <div className="h-full w-fit overflow-auto">{component}</div>
         </div>
       </div>
     </div>
